4a54ab04b26277e0ce5f215509b844ec30b8585a
Duy Anh <duyanh.it.coder@gmail.com>
2022-05-10 00:26:28 +0700

    Learning Module

---
+++ learningModule.js
@@ -0,0 +1,13 @@
+var countArr = require("./module.js")
+
+var arr = [
+    "abc", 
+    123, 
+    true, 
+    {
+        name: "Duy Anh",
+        age: 43
+    }
+]
+
+console.log(countArr(arr))
---
+++ module.js
@@ -0,0 +1,18 @@
+
+// var arr = [
+//     "abc", 
+//     123, 
+//     true, 
+//     {
+//         name: "Duy Anh",
+//         age: 43
+//     }
+// ]
+
+var countArr = function(array)  {
+    return `There are ${array.length} element in Array`
+}
+
+// console.log(countArr(arr))
+
+module.exports = countArr
